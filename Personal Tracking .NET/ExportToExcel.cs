1af3f483e3ce3c34e810a7eb880740c462362f96
Phạm Tiến Dũng <56197293+phamtiendungcwit@users.noreply.github.com>
2022-02-27 16:54:37 +0700

    Update export

--- Personal Tracking .NET/ExportToExcel.cs
+++ Personal Tracking .NET/ExportToExcel.cs
@@ -27,7 +27,14 @@ namespace Personal_Tracking.NET
             {
                 for (int j = 0; j < dataGridView1.Columns.Count; j++)
                 {
-                    worksheet.Cells[i + 2, j + 1] = dataGridView1.Rows[i].Cells[j].Value.ToString();
+                    if (dataGridView1.Rows[i].Cells[j].Value != null)
+                    {
+                        worksheet.Cells[i + 2, j + 1] = dataGridView1.Rows[i].Cells[j].Value.ToString();
+                    }
+                    else
+                    {
+                        worksheet.Cells[i + 2, j + 1] = "";
+                    }
                 }
             }
 
