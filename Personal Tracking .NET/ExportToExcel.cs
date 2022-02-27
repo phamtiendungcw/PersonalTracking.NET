4a7b92e8403f9dace051f5ed165b43825648948b
Phạm Tiến Dũng <56197293+phamtiendungcwit@users.noreply.github.com>
2022-02-27 16:36:24 +0700

    Update Export

--- Personal Tracking .NET/ExportToExcel.cs
+++ Personal Tracking .NET/ExportToExcel.cs
@@ -9,7 +9,7 @@ namespace Personal_Tracking.NET
 {
     public class ExportToExcel
     {
-        public static void ExcelExport(DataGridView dataGridView)
+        public static void ExcelExport(DataGridView dataGridView1)
         {
             Microsoft.Office.Interop.Excel._Application app = new Microsoft.Office.Interop.Excel.Application();
             Microsoft.Office.Interop.Excel._Workbook workbook = app.Workbooks.Add(Type.Missing);
@@ -18,15 +18,15 @@ namespace Personal_Tracking.NET
             //worksheet = workbook.Sheets["Trang_tính1"];     // Language = vn
             worksheet = workbook.Sheets["Sheet1"];          // Language = en
             worksheet = workbook.ActiveSheet;
-            for (int i = 1; i < dataGridView.Columns.Count + 1; i++)
+            for (int i = 1; i < dataGridView1.Columns.Count + 1; i++)
             {
-                worksheet.Cells[1, i] = dataGridView.Columns[i - 1].HeaderText;
+                worksheet.Cells[1, i] = dataGridView1.Columns[i - 1].HeaderText;
             }
-            for (int i = 0; i < dataGridView.Rows.Count; i++)
+            for (int i = 0; i < dataGridView1.Rows.Count - 1; i++)
             {
-                for (int j = 0; j < dataGridView.Columns.Count; j++)
+                for (int j = 0; j < dataGridView1.Columns.Count; j++)
                 {
-                    worksheet.Cells[i + 2, j + 1] = dataGridView.Rows[i].Cells[j].Value.ToString();
+                    worksheet.Cells[i + 2, j + 1] = dataGridView1.Rows[i].Cells[j].Value.ToString();
                 }
             }
 
